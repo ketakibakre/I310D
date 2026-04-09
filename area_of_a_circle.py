5fd642307df649bfa0e813cc4ae47ff2d26c722a
ketaki bakre <ketakibakre@MacBook-Air-2.local>
2026-04-08 20:09:15 -0500

    added in line comments

--- area_of_a_circle.py
+++ area_of_a_circle.py
@@ -1,12 +1,14 @@
+# Hi, this program calculates the area of a circle, given the radius 
+
 def compute_area_of_circle(radius):
 	pi = 3.14
 	area = pi * radius * radius
 	return area
 
-radius1 = 30
+radius1 = 30 # the area of circle 1
 area1 = compute_area_of_circle(radius1)
 print(f"The area of circle with radius {radius1} is: {area1}")
 
-radius2 = 40
+radius2 = 40 # the area of circle 2
 area2 = compute_area_of_circle(radius2)
 print(f"The area of circle with radius {radius2} is: {area2}")
